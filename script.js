708543c352dcc599e9709d3d3d01704e4401924f
David <devdavee@gmail.com>
2018-05-16 09:00:37 -0400

    added comments and variable name change

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-const css = document.querySelector("h3");
+// Variables 
+const h3 = document.querySelector("h3");
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 const body = document.getElementById("gradient");
 
+// setGradient function is created to display color values within the h3
 const gradient = (setGradient) => {
     body.style.background = "linear-gradient(to right, " + color1.value
         + ", "
         + color2.value
         + ")";
 
-        css.textContent = body.style.background + ";";
+        h3.textContent = body.style.background + ";";
 }
 
+// Selects both color inputs and applies the gradient variable holding the setGradient function
 color1.addEventListener("input", gradient);
 
 color2.addEventListener("input", gradient);
